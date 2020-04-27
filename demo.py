8121a1c31e31820f579ec1aa971621512e44fef1
Melissa Mullen <mullen@Melissas-MacBook-Pro.local>
2020-04-27 00:44:04 -0400

    implemented top_five(), fixed other methods: created count_data() to count categorized data adjusted categorize_data()

--- demo.py
+++ demo.py
@@ -6,50 +6,80 @@ def getColumnsNames(filename):
 
 
 def group_by_category(filename):
-    data_columns = getColumnsNames(filename)
-    target_column_index = 4
-    with open('stats.csv', 'r') as file_ref:
-        temp_frequency_holder_dic = {}
-        frequency_data = {
-            'min_salary': {'count': 0, 'min': 0, 'max': 50000, 'data': {}},
-            'mid_salary': {'count': 0, 'min': 50001, 'max': 80000, 'data': {}},
-            'hig_salary':  {'count': 0, 'min': 80001, 'max': 200000, 'data': {}}
-        }
+# creating an array of the columns name in the csv file
+data_columns = DeveloperStats.get_column_names(filename)
+# target index for salary column
+target_column_index = 4
+# index for language column
+programming_languages_column = 12
+# opening the data file
+with open('stats.csv', 'r') as file_ref:
+    # holding the frequency in which the items of the row appear this is temporal
+    # bcause is going to be assigned to one of the groups we created below
+    temp_frequency_holder_dic = {}
+    # groups to hold the data, and has min and max baundaries as well count for analytics
+    frequency_data = {
+        'low_salary': {'count': 0, 'min': 0, 'max': 50000, 'data': {}},
+        'medium_salary': {'count': 0, 'min': 50001, 'max': 80000, 'data': {}},
+        'high_salary':  {'count': 0, 'min': 80001, 'max': 200000, 'data': {}}
+    }
+    # reading csv file, skiping first line bc is header (contains the column's name)
+    for line in file_ref.readlines()[1:]:
+        # create an array fields split by '|'
+        row = line.strip().split('|')
+        # taking the float value of the salary filed
+        field_salary = float(row[target_column_index])
 
-        for line in file_ref.readlines()[1:]:
-
-            row = line.strip().split('|')
-
-            field_salary = float(row[target_column_index])
-
-            for index, field in enumerate(row):
-                column_name = data_columns[index]
+        # geting the element and index in every iteration
+        for index, field in enumerate(row):
+            # get the column name from array using index
+            column_name = data_columns[index]
+            # check if the column name is not there, then create one
+            if(index not in [0, 4]):
                 if(column_name not in temp_frequency_holder_dic):
                     temp_frequency_holder_dic[column_name] = {}
 
-                # it's skipping language for now """"if(index not in [0, 4, 12]): this is for skiping programming languages( for now) and along with salary and responded which are not needed"""
+            # now that the column name it's created, filter data (skip 0=Respondent, and 4="salary", being that whe don't need to add salary to data, bc is already selected in 'field_salary' at the begining)
+            if(index not in [0, 4, 12]):
+                # if that field value is not within our data from the column in the temp variable then create one
+                if(field not in temp_frequency_holder_dic[column_name]):
+                    # create the field name set it to 0
+                    temp_frequency_holder_dic[column_name][field] = 0
+                # now that we have the field name just increment it by 1
+                temp_frequency_holder_dic[column_name][field] += 1
+            if(index == programming_languages_column):
+                # first check if is a special case as programming languages (this is kinda hardcoded, the optimal would be with a
+                # method that says whether or not the field contains multiple value as programming laguanges field does, for that
+                # just replace the line of code below for if (not hasMultipleValues(temp_frequency_holder_dic[column_name][field])))
+                # iterate through every field
+                list_of_languages = field.split(';')
+                for language in list_of_languages:
+                    # now verify if this language is in upper level of the dictionary structure
+                    if(language not in temp_frequency_holder_dic[column_name]):
+                        # if is not there, then create one
+                        temp_frequency_holder_dic[column_name][language] = 0
+                    # increment it by 1
+                    temp_frequency_holder_dic[column_name][language] += 1
+        # these statements allocate the temp dictionary into the corresponding group
+        # this repeats in each line the csv so that each row is sorted into a group
+        if(field_salary < frequency_data['medium_salary']['min']):
 
-                if(index not in [0, 4, 12]):
-                    if(field not in temp_frequency_holder_dic[column_name]):
-                        temp_frequency_holder_dic[column_name][field] = 0
-                    temp_frequency_holder_dic[column_name][field] += 1
+            frequency_data['low_salary']['data'] = temp_frequency_holder_dic
+            frequency_data['low_salary']['count'] += 1
 
-            if(field_salary < frequency_data['mid_salary']['min']):
+        elif(field_salary < frequency_data['high_salary']['min']):
 
-                frequency_data['min_salary']['data'] = temp_frequency_holder_dic
-                frequency_data['min_salary']['count'] += 1
+            frequency_data['medium_salary']['data'] = temp_frequency_holder_dic
+            frequency_data['medium_salary']['count'] += 1
 
-            elif(field_salary < frequency_data['hig_salary']['min']):
+        else:
 
-                frequency_data['mid_salary']['data'] = temp_frequency_holder_dic
-                frequency_data['mid_salary']['count'] += 1
+            frequency_data['high_salary']['data'] = temp_frequency_holder_dic
+            frequency_data['high_salary']['count'] += 1
 
-            else:
+# now we return the data
+return frequency_data
 
-                frequency_data['hig_salary']['data'] = temp_frequency_holder_dic
-                frequency_data['hig_salary']['count'] += 1
-
-    return frequency_data
 
 
 def demo(filename):
--- project.py
+++ project.py
@@ -26,10 +26,57 @@ class DeveloperStats():
     @classmethod
     def categorize_data(cls, filename):
         """
-        This method sorts through the CSV file provided from filename,
-        categorizes the data into three categories: low_salary, medium_salary,
-        and high_salary, and counts the occurrance of respondant's answers if __name__ == '__main__':
-        each salary category
+        This method sorts through the CSV file provided from filename and sorts
+        the responses into three categories (low_salary, medium_salary,
+        high_salary) based on their salary
+        filename: name of a CSV file (string)
+        returns: dictionary
+            keys: salary category (string)
+            values: dictionary
+                keys: summary data from the method calculation (including count
+                of responses, min salary, max salary, and data) (strings)
+                values: the count of respondants (integer), min salary
+                (integer), max salary (integer), and a list of lists containing
+                responses
+        """
+        data_columns = DeveloperStats.get_column_names(filename)
+        target_column_index = 4
+        low_salary_list = []
+        medium_salary_list = []
+        high_salary_list = []
+        frequency_data = {
+            'low_salary': {'count': 0, 'min': 0, 'max': 50000, 'data': {}},
+            'medium_salary': {'count': 0, 'min': 50001, 'max': 80000, 'data': {}},
+            'high_salary':  {'count': 0, 'min': 80001, 'max': 200000, 'data': {}}
+        }
+        with open('stats.csv', 'r') as file_ref:
+            temp_frequency_holder_dic = {}
+            for line in file_ref.readlines()[1:]:
+                row = line.strip().split('|')
+                field_salary = float(row[target_column_index])
+                del row[0] # respondant id
+                del row[3] # salary
+                del row[4] # gender
+                if field_salary <= 50000:
+                    low_salary_list.append(row) # appends this response to low_salary_list
+                    frequency_data['low_salary']['count'] += 1 # count increases
+                elif field_salary <= 80000:
+                    medium_salary_list.append(row)
+                    frequency_data['medium_salary']['count'] += 1
+                else:
+                    high_salary_list.append(row)
+                    frequency_data['high_salary']['count'] += 1
+        frequency_data['low_salary']['data'] = low_salary_list
+        frequency_data['medium_salary']['data'] = medium_salary_list
+        frequency_data['high_salary']['data'] = high_salary_list
+
+        return frequency_data
+
+    @classmethod
+    def count_data(cls, filename):
+        """
+        This method sorts through the CSV file provided from filename and
+        counts the occurrance of respondant's answers in each salary category
         filename: name of a CSV file (string)
         Returns: dictionary
             keys: salary category (strings)
@@ -44,131 +91,91 @@ class DeveloperStats():
                         keys: answers from respondents (strings)
                         values: count of occurrence of answers (integers)
         """
-        # creating an array of the columns name in the csv file
-        data_columns = DeveloperStats.get_column_names(filename)
-        # target index for salary column
-        target_column_index = 4
-        # index for language column
-        programming_languages_column = 12
-        # opening the data file
-        with open('stats.csv', 'r') as file_ref:
-            # holding the frequency in which the items of the row appear this is temporal
-            # bcause is going to be assigned to one of the groups we created below
+        category_dict = DeveloperStats.categorize_data(filename)
+        frequency_data = {
+            'low_salary': {'count': 0, 'min': 0, 'max': 50000, 'data': {}},
+            'medium_salary': {'count': 0, 'min': 50001, 'max': 80000, 'data': {}},
+            'high_salary':  {'count': 0, 'min': 80001, 'max': 200000, 'data': {}}
+        }
+        for salary_category in category_dict:
+            frequency_data[salary_category]['count'] = category_dict[salary_category]['count'] # makes the count values the same as the previous method
             temp_frequency_holder_dic = {}
-            # groups to hold the data, and has min and max baundaries as well count for analytics
-            frequency_data = {
-                'low_salary': {'count': 0, 'min': 0, 'max': 50000, 'data': {}},
-                'medium_salary': {'count': 0, 'min': 50001, 'max': 80000, 'data': {}},
-                'high_salary':  {'count': 0, 'min': 80001, 'max': 200000, 'data': {}}
-            }
-            # reading csv file, skiping first line bc is header (contains the column's name)
-            for line in file_ref.readlines()[1:]:
-                # create an array fields split by '|'
-                row = line.strip().split('|')
-                # taking the float value of the salary filed
-                field_salary = float(row[target_column_index])
+            for responses in category_dict[salary_category]['data']: # for responses in each salary category
+                languages = responses[-1] # picks out the LanguageWorkedWith column
+                list_of_languages = languages.split(';') # parses it
+                for language in list_of_languages:
+                    if(language not in temp_frequency_holder_dic): # if it's not a key already...
+                        temp_frequency_holder_dic[language] = 0 # make it a key value pair
+                    temp_frequency_holder_dic[language] += 1 # increment by one with each occurrance
+                for item in responses[:-1]: # for all other features except languages
+                    if(item not in temp_frequency_holder_dic):
+                        temp_frequency_holder_dic[item] = 0
+                    temp_frequency_holder_dic[item] += 1
+            frequency_data[salary_category]['data'] = temp_frequency_holder_dic
 
-                # geting the element and index in every iteration
-                for index, field in enumerate(row):
-                    # get the column name from array using index
-                    column_name = data_columns[index]
-                    # check if the column name is not there, then create one
-                    if(index not in [0, 4]):
-                        if(column_name not in temp_frequency_holder_dic):
-                            temp_frequency_holder_dic[column_name] = {}
-
-                    # now that the column name it's created, filter data (skip 0=Respondent, and 4="salary", being that whe don't need to add salary to data, bc is already selected in 'field_salary' at the begining)
-                    if(index not in [0, 4, 12]):
-                        # if that field value is not within our data from the column in the temp variable then create one
-                        if(field not in temp_frequency_holder_dic[column_name]):
-                            # create the field name set it to 0
-                            temp_frequency_holder_dic[column_name][field] = 0
-                        # now that we have the field name just increment it by 1
-                        temp_frequency_holder_dic[column_name][field] += 1
-                    if(index == programming_languages_column):
-                        # first check if is a special case as programming languages (this is kinda hardcoded, the optimal would be with a
-                        # method that says whether or not the field contains multiple value as programming laguanges field does, for that
-                        # just replace the line of code below for if (not hasMultipleValues(temp_frequency_holder_dic[column_name][field])))
-                        # iterate through every field
-                        list_of_languages = field.split(';')
-                        for language in list_of_languages:
-                            # now verify if this language is in upper level of the dictionary structure
-                            if(language not in temp_frequency_holder_dic[column_name]):
-                                # if is not there, then create one
-                                temp_frequency_holder_dic[column_name][language] = 0
-                            # increment it by 1
-                            temp_frequency_holder_dic[column_name][language] += 1
-                # these statements allocate the temp dictionary into the corresponding group
-                # this repeats in each line the csv so that each row is sorted into a group
-                if(field_salary < frequency_data['medium_salary']['min']):
-
-                    frequency_data['low_salary']['data'] = temp_frequency_holder_dic
-                    frequency_data['low_salary']['count'] += 1
-
-                elif(field_salary < frequency_data['high_salary']['min']):
-
-                    frequency_data['medium_salary']['data'] = temp_frequency_holder_dic
-                    frequency_data['medium_salary']['count'] += 1
-
-                else:
-
-                    frequency_data['high_salary']['data'] = temp_frequency_holder_dic
-                    frequency_data['high_salary']['count'] += 1
-
-        # now we return the data
         return frequency_data
-
-
     @classmethod
     def top_five(cls, category_dict):
         """
         This method sorts through the dictionary returned from
-        categorize_data() and determines the top five results for each salary
+        count_data() and determines the top five results for each salary
         category
-        category_dict: the dictionary returned from categorize_data()
+        category_dict: the dictionary returned from count_data()
         Returns: dictionary
             keys: salary category (strings)
             values: dictionary
-                keys: categories from CSV file (strings)
+                keys: rankings (strings)
                 values: dictionary
-                    keys: top answers (strings)
-                    values: count of occurrence of answers (integers)
+                    keys: name of feature (string)
+                    values: count of occurrence of feature (integer)
         """
         top_five_dict = {}
         for key in category_dict.keys():
             top_five_dict[key] = {}
             temp_count = {'one': 0, 'two': 0, 'three': 0, 'four': 0, 'five': 0}
+            for category in category_dict[key]['data']: # for responses in each salary category
+                item_count = (category_dict[key]['data'][category]) # count of feature
+                if item_count > temp_count['one']: # if count is greater than largest feature
+                    # move all the other features down a notch
+                    temp_count['five'] = temp_count['four']
+                    temp_count['four'] = temp_count['three']
+                    temp_count['three'] = temp_count['two']
+                    temp_count['two'] = temp_count['one']
+                    temp_count['one'] = item_count # make this feature the largest feature
 
-            for category in category_dict[key]['data']:
-                for item in category_dict[key]['data'][category]:
-                    item_count = (category_dict[key]['data'][category][item])
-                    if item_count > temp_count['one']:
-                        temp_count['five'] = temp_count['four']
-                        temp_count['four'] = temp_count['three']
-                        temp_count['three'] = temp_count['two']
-                        temp_count['two'] = temp_count['one']
-                        temp_count['one'] = item_count
-                    elif item_count > temp_count['two']:
-                        temp_count['five'] = temp_count['four']
-                        temp_count['four'] = temp_count['three']
-                        temp_count['three'] = temp_count['two']
-                        temp_count['two'] = item_count
-                    elif item_count > temp_count['three']:
-                        temp_count['five'] = temp_count['four']
-                        temp_count['four'] = temp_count['three']
-                        temp_count['three'] = item_count
-                    elif item_count > temp_count['four']:
-                        temp_count['five'] = temp_count['four']
-                        temp_count['four'] = item_count
-                    elif item_count > temp_count['five']:
-                        temp_count['five'] = item_count
+                elif item_count > temp_count['two']:
+                    temp_count['five'] = temp_count['four']
+                    temp_count['four'] = temp_count['three']
+                    temp_count['three'] = temp_count['two']
+                    temp_count['two'] = item_count
+
+                elif item_count > temp_count['three']:
+                    temp_count['five'] = temp_count['four']
+                    temp_count['four'] = temp_count['three']
+                    temp_count['three'] = item_count
+
+                elif item_count > temp_count['four']:
+                    temp_count['five'] = temp_count['four']
+                    temp_count['four'] = item_count
+
+                elif item_count > temp_count['five']:
+                    temp_count['five'] = item_count
+
+            keys = list(category_dict[key]['data'].keys()) # a list of the keys
+            values = list(category_dict[key]['data'].values()) # a list of the values
+
+            one = keys[values.index(temp_count['one'])] # finding the key associated with the value pair
+            two = keys[values.index(temp_count['two'])]
+            three = keys[values.index(temp_count['three'])]
+            four = keys[values.index(temp_count['four'])]
+            five = keys[values.index(temp_count['five'])]
+
             top_five_dict[key] = temp_count
-            # for category in category_dict[key]['data']:
-            #     if category not in top_five_dict[key]:
-            #         top_five_dict[key][category] = {}
-            #     for items in category_dict[key]['data'][category]:
-            #         top_five_dict[key][category][items] = category_dict[key]['data'][category][items]
-                    # print(category_dict[key]['data'][category][items])
+            top_five_dict[key]['one'] = {one: temp_count['one']}
+            top_five_dict[key]['two'] = {two: temp_count['two']}
+            top_five_dict[key]['three'] = {three: temp_count['three']}
+            top_five_dict[key]['four'] = {four: temp_count['four']}
+            top_five_dict[key]['five'] = {five: temp_count['five']}
 
         return top_five_dict
 
@@ -190,15 +197,19 @@ def main():
     develop = DeveloperStats()
     filename = 'stats.csv'
     result = develop.get_column_names(filename)
-    # print(f' column names in {filename} are: {result}')
-    # print("\n")
-    #
-    category_dict = develop.categorize_data(filename)
-    print(f' example {filename} are: {category_dict}')
+    print(f' column names in {filename} are: {result}')
+    print("\n")
+
+    result = develop.categorize_data(filename)
+    print(f' categorized data in {filename} is: {result}')
+    print("\n")
+
+    category_dict = develop.count_data(filename)
+    print(f' counted categories in {filename} are: {category_dict}')
     print("\n")
 
     top_five_dict = develop.top_five(category_dict)
-    print(f' {top_five_dict}')
+    print(f' the top five occurring features are {top_five_dict}')
 
 
 if __name__ == '__main__':
